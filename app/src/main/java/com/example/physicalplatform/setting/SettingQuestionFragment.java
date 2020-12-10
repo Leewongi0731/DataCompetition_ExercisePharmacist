35459a8b3f2324b505f537847ddd6892d81bff86
lee wongi <48685242+Leewongi0731@users.noreply.github.com>
2020-12-10 11:26:06 +0900

    setting layout fragment commit

---
+++ app/src/main/java/com/example/physicalplatform/setting/SettingQuestionFragment.java
@@ -0,0 +1,42 @@
+package com.example.physicalplatform.setting;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.physicalplatform.R;
+
+public class SettingQuestionFragment extends Fragment {
+    private ViewGroup viewGroup;
+    private Context context;
+
+    private Button settingQuestionsBackBtn;
+
+    private FragmentManager fragmentManager;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.setting_questions, container, false);
+        context = container.getContext();
+
+
+        // set backBtn
+        settingQuestionsBackBtn = (Button) viewGroup.findViewById(R.id.settingQuestionsBackBtn);
+        settingQuestionsBackBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.popBackStackImmediate();
+            }
+        });
+
+
+        return viewGroup;
+    }
+}
