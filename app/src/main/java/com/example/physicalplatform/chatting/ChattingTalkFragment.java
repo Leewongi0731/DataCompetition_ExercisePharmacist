737c60bb917c33eb7cae90cd9c9b7b790e620714
YoungBaeKim <canoe918@gmail.com>
2020-12-10 18:55:01 +0900

    modify matching and chatting xml layout and add function change fragment

--- app/src/main/java/com/example/physicalplatform/chatting/ChattingTalkFragment.java
+++ app/src/main/java/com/example/physicalplatform/chatting/ChattingTalkFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,14 +21,19 @@ import com.example.physicalplatform.data.MatchingListDataset;
 
 import java.util.ArrayList;
 
-public class ChattingTalkFragment extends Fragment {
+public class ChattingTalkFragment extends Fragment implements View.OnClickListener {
     private ViewGroup viewGroup;
     private Context context;
+
+    private ImageView imageViewTalkBackBtn;
 
     private RecyclerView chattingRecyclerView;
     private RecyclerView.LayoutManager chattingLayoutManager;
     private ChattingFragmentRecyclerViewAdapter chattingAdapter;
     private ChattingListDataset chattingListDataset;
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
 
     public ChattingTalkFragment(ChattingListDataset chattingListDataset) {
         this.chattingListDataset = chattingListDataset;
@@ -39,11 +47,15 @@ public class ChattingTalkFragment extends Fragment {
 
         initLayout();
 
+        imageViewTalkBackBtn.setOnClickListener(this);
+
         return viewGroup;
     }
 
     private void initLayout() {
         chattingRecyclerView = viewGroup.findViewById(R.id.recyclerViewChatting);
+
+        imageViewTalkBackBtn = viewGroup.findViewById(R.id.imageViewTalkBackBtn);
 
 //        chattingAdapter = new ChattingFragmentRecyclerViewAdapter(context, chattingListDatasetsFiltered);
 //        chattingRecyclerView.setAdapter(chattingAdapter);
@@ -51,5 +63,17 @@ public class ChattingTalkFragment extends Fragment {
 //
 //        chattingLayoutManager = new LinearLayoutManager(context);
 //        chattingRecyclerView.setLayoutManager(chattingLayoutManager);
+
+        fragmentManager = getFragmentManager();
+        transaction = fragmentManager.beginTransaction();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.imageViewTalkBackBtn:
+                fragmentManager.popBackStackImmediate();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/physicalplatform/matching/MatchingDetailFragment.java
+++ app/src/main/java/com/example/physicalplatform/matching/MatchingDetailFragment.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.physicalplatform.R;
 import com.example.physicalplatform.data.MatchingListDataset;
@@ -33,15 +36,26 @@ public class MatchingDetailFragment extends Fragment implements View.OnClickList
     private TextView textViewCurriculum;
     private TextView textViewClassPlan;
     private TextView textViewClassReview;
-    private TextView textViewContents;
 
     private View viewIntroduction;
     private View viewCurriculum;
     private View viewClassPlan;
     private View viewClassReview;
 
-    private Button buttonQuestion;
-    private Button buttonRegister;
+    private LinearLayout linearLayoutIntroduction;
+    private TextView textViewIntroductionContents;
+    private TextView buttonIntroductionQuestion;
+    private TextView buttonIntroductionRegister;
+
+    private LinearLayout linearLayoutCurriculum;
+    private TextView textViewCurriculumContents;
+
+    private LinearLayout linearLayoutClassPlan;
+    private TextView textViewClassPlanSummary;
+    private CalendarView calendarViewClassPlan;
+
+    private LinearLayout linearLayoutClassReview;
+    private RecyclerView recyclerViewClassReviewList;
 
     private MatchingListDataset matchingListDataset;
 
@@ -72,8 +86,8 @@ public class MatchingDetailFragment extends Fragment implements View.OnClickList
         textViewClassPlan.setOnClickListener(this);
         textViewClassReview.setOnClickListener(this);
 
-        buttonQuestion.setOnClickListener(this);
-        buttonRegister.setOnClickListener(this);
+        buttonIntroductionQuestion.setOnClickListener(this);
+        buttonIntroductionRegister.setOnClickListener(this);
 
         return viewGroup;
     }
@@ -91,15 +105,26 @@ public class MatchingDetailFragment extends Fragment implements View.OnClickList
         textViewCurriculum = viewGroup.findViewById(R.id.textViewCurriculum);
         textViewClassPlan = viewGroup.findViewById(R.id.textViewClassPlan);
         textViewClassReview = viewGroup.findViewById(R.id.textViewClassReview);
-        textViewContents = viewGroup.findViewById(R.id.textViewContents);
 
         viewIntroduction = viewGroup.findViewById(R.id.viewIntroduction);
         viewCurriculum = viewGroup.findViewById(R.id.viewCurriculum);
         viewClassPlan = viewGroup.findViewById(R.id.viewClassPlan);
         viewClassReview = viewGroup.findViewById(R.id.viewClassReview);
 
-        buttonQuestion = viewGroup.findViewById(R.id.buttonQuestion);
-        buttonRegister = viewGroup.findViewById(R.id.buttonRegister);
+        linearLayoutIntroduction = viewGroup.findViewById(R.id.linearLayoutIntroduction);
+        textViewIntroductionContents = viewGroup.findViewById(R.id.textViewIntroductionContents);
+        buttonIntroductionQuestion = viewGroup.findViewById(R.id.buttonIntroductionQuestion);
+        buttonIntroductionRegister = viewGroup.findViewById(R.id.buttonIntroductionRegister);
+
+        linearLayoutCurriculum = viewGroup.findViewById(R.id.linearLayoutCurriculum);
+        textViewCurriculumContents = viewGroup.findViewById(R.id.textViewCurriculumContents);
+
+        linearLayoutClassPlan = viewGroup.findViewById(R.id.linearLayoutClassPlan);
+        textViewClassPlanSummary = viewGroup.findViewById(R.id.textViewClassPlanSummary);
+        calendarViewClassPlan = viewGroup.findViewById(R.id.calendarViewClassPlan);
+
+        linearLayoutClassReview = viewGroup.findViewById(R.id.linearLayoutClassReview);
+        recyclerViewClassReviewList = viewGroup.findViewById(R.id.recyclerViewClassReviewList);
 
         fragmentManager = getFragmentManager();
         transaction = fragmentManager.beginTransaction();
@@ -112,29 +137,27 @@ public class MatchingDetailFragment extends Fragment implements View.OnClickList
                 fragmentManager.popBackStackImmediate();
                 break;
             case R.id.textViewIntroduction:
-                setTextSelected("소개", R.color.custom_red, R.color.black, R.color.black, R.color.black, View.VISIBLE, View.INVISIBLE, View.INVISIBLE, View.INVISIBLE);
+                setTextSelected(R.color.custom_red, R.color.black, R.color.black, R.color.black, View.VISIBLE, View.GONE, View.GONE, View.GONE);
                 break;
             case R.id.textViewCurriculum:
-                setTextSelected("커리큘럼", R.color.black, R.color.custom_red, R.color.black, R.color.black, View.INVISIBLE, View.VISIBLE, View.INVISIBLE, View.INVISIBLE);
+                setTextSelected(R.color.black, R.color.custom_red, R.color.black, R.color.black, View.GONE, View.VISIBLE, View.GONE, View.GONE);
                 break;
             case R.id.textViewClassPlan:
-                setTextSelected("일정", R.color.black, R.color.black, R.color.custom_red, R.color.black, View.INVISIBLE, View.INVISIBLE, View.VISIBLE, View.INVISIBLE);
+                setTextSelected(R.color.black, R.color.black, R.color.custom_red, R.color.black, View.GONE, View.GONE, View.VISIBLE, View.GONE);
                 break;
             case R.id.textViewClassReview:
-                setTextSelected("후기", R.color.black, R.color.black, R.color.black, R.color.custom_red, View.INVISIBLE, View.INVISIBLE, View.INVISIBLE, View.VISIBLE);
+                setTextSelected(R.color.black, R.color.black, R.color.black, R.color.custom_red, View.GONE, View.GONE, View.GONE, View.VISIBLE);
                 break;
-            case R.id.buttonQuestion:
+            case R.id.buttonIntroductionQuestion:
 
                 break;
-            case R.id.buttonRegister:
+            case R.id.buttonIntroductionRegister:
 
                 break;
         }
     }
 
-    private void setTextSelected(String content, Integer c1, Integer c2, Integer c3, Integer c4, Integer v1, Integer v2, Integer v3, Integer v4) {
-        textViewContents.setText(content);
-
+    private void setTextSelected(Integer c1, Integer c2, Integer c3, Integer c4, Integer v1, Integer v2, Integer v3, Integer v4) {
         textViewIntroduction.setTextColor(getResources().getColor(c1));
         textViewCurriculum.setTextColor(getResources().getColor(c2));
         textViewClassPlan.setTextColor(getResources().getColor(c3));
@@ -144,6 +167,11 @@ public class MatchingDetailFragment extends Fragment implements View.OnClickList
         viewCurriculum.setVisibility(v2);
         viewClassPlan.setVisibility(v3);
         viewClassReview.setVisibility(v4);
+
+        linearLayoutIntroduction.setVisibility(v1);
+        linearLayoutCurriculum.setVisibility(v2);
+        linearLayoutClassPlan.setVisibility(v3);
+        linearLayoutClassReview.setVisibility(v4);
     }
 }
 
